1d28e8fa7553fb93da3c05c1ae5495b42e3bf48d
ricky <rickkkkkkkkky@foxmail.com>
2020-06-23 11:34:47 +0800

    test: update test style

--- test/index.js
+++ test/index.js
@@ -1,6 +1,4 @@
 const test = require('promises-aplus-tests')
 const adapter = require('./adapter')
-
 console.log('\n\x1B[36m %s \x1B[39m\n', 'start testing...')
-
-test(adapter, { reporter: 'nyan' })
+test(adapter, { reporter: 'landing' })
