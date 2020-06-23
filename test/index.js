026a980ce35480181353f8ca8a21f7497095eb5c
ricky <rickkkkkkkkky@foxmail.com>
2020-06-23 15:16:24 +0800

    test: fix test cli style

--- test/index.js
+++ test/index.js
@@ -1,4 +1,4 @@
 const test = require('promises-aplus-tests')
 const adapter = require('./adapter')
-console.log('\n\x1B[36m %s \x1B[39m\n', 'start testing...')
+console.log('\n\x1B[36m %s \x1B[39m\n', ' start testing...')
 test(adapter, { reporter: 'landing' })
